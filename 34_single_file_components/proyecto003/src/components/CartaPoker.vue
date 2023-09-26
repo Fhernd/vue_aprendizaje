c0f95c8bb4dbb84cb628bb621a267d66ebff50c7
John Ortiz Ordoñez <johnortizo@outlook.com>
2023-09-26 11:21:25 -0500

    Componente de carta de póker

---
+++ 34_single_file_components/proyecto003/src/components/CartaPoker.vue
@@ -0,0 +1,30 @@
+<template>
+    <span class="carta-poker">
+        {{ valor }}<br>{{ nombre[tipo] }}
+    </span>
+</template>
+  
+<script>
+export default {
+    data() {
+        return {
+            tipo: Math.trunc(Math.random() * 4),
+            valor: Math.trunc(Math.random() * 12) + 1,
+            nombre: ['pica', 'corazon', 'diamante', 'trébol']
+        }
+    }
+}
+</script>
+  
+<style>
+.carta-poker {
+    display: inline-block;
+    background-color: black;
+    color: white;
+    border-radius: 5px;
+    width: 8rem;
+    height: 13rem;
+    font-size: 2rem;
+    text-align: center
+}
+</style>
