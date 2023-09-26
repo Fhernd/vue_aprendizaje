2e6746cfcfdb13be5994429252ffb6eebca645cc
John Ortiz Ordoñez <johnortizo@outlook.com>
2023-09-26 14:45:12 -0500

    Uso de la componente CartaPoker de Vue

--- 34_single_file_components/proyecto003/src/App.vue
+++ 34_single_file_components/proyecto003/src/App.vue
@@ -1,11 +1,20 @@
 <template>
   <div id="app">
     <h1>{{ mensaje }}</h1>
+    <carta-poker></carta-poker>
+    <carta-poker></carta-poker>
+    <carta-poker></carta-poker>
+    <carta-poker></carta-poker>
+    <carta-poker></carta-poker>
   </div>
 </template>
 
 <script>
+import CartaPoker from './components/CartaPoker.vue'
 export default {
+  components: {
+    'carta-poker': CartaPoker
+  },
   name: 'app',
   data() {
     return {
