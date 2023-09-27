64b1e1477e00c28ded74940d0262e7a6ec5720dd
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2023-09-27 18:19:32 -0500

    Using flat arrow functions

--- vue3_course/app001_counter_js/index.js
+++ vue3_course/app001_counter_js/index.js
@@ -2,12 +2,12 @@ window.onload = function () {
     const btnAdd = document.getElementById('add');
     const btnSub = document.getElementById('sub');
 
-    btnAdd.addEventListener('click', function () {
+    btnAdd.addEventListener('click', () => {
         const counter = document.getElementById('count');
         counter.innerText = parseInt(counter.innerText) + 1;
     });
 
-    btnSub.addEventListener('click', function () {
+    btnSub.addEventListener('click', () => {
         const counter = document.getElementById('count');
         counter.innerText = parseInt(counter.innerText) - 1;
     });
