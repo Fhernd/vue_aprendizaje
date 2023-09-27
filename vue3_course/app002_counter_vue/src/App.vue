f691c11caf19489caa6afb7190027cf7fc894464
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2023-09-27 18:35:27 -0500

    Counter

--- vue3_course/app002_counter_vue/src/App.vue
+++ vue3_course/app002_counter_vue/src/App.vue
@@ -1,10 +1,16 @@
+<script setup>
+  import { ref } from 'vue';
+
+  const count = ref(0);
+</script>
+
 <template>
   <main>
     <div>
       <h4>
         The current count is...
       </h4>
-      <h1>0</h1>
+      <h1>{{ count }}</h1>
       <button>-</button>
       <button>+</button>
     </div>
