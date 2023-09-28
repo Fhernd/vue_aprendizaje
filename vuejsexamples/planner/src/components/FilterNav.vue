7df877eabb4ad9b7b1499023c27be6b35d4ddb20
John Ortiz Ordoñez <johnortizo@outlook.com>
2023-09-28 11:30:23 -0500

    Formatting

--- vuejsexamples/planner/src/components/FilterNav.vue
+++ vuejsexamples/planner/src/components/FilterNav.vue
@@ -1,8 +1,8 @@
 <template>
     <nav class="filter-nav">
-        <button @click="updateFilter('all')" :class="{active:current === 'all'}">All Project</button>
-        <button @click="updateFilter('completed')" :class="{active:current === 'completed'}">Completed Project</button>
-        <button @click="updateFilter('ongoing')" :class="{active:current === 'ongoing'}">Ongoing Project</button>
+        <button @click="updateFilter('all')" :class="{ active: current === 'all' }">All Project</button>
+        <button @click="updateFilter('completed')" :class="{ active: current === 'completed' }">Completed Project</button>
+        <button @click="updateFilter('ongoing')" :class="{ active: current === 'ongoing' }">Ongoing Project</button>
     </nav>
 </template>
 
@@ -18,19 +18,20 @@ export default {
 </script>
 
 <style>
-    .filter-nav button {
-        background: none;
-        border: none;
-        outline: none;
-        color: #bbb;
-        font-size: 15px;
-        text-transform: uppercase;
-        margin-right: 10px;
-        letter-spacing: 1px;
-        font-weight: bold;
-        cursor: pointer;
-    }
-    .filter-nav button.active {
-        color: #555;
-    }
+.filter-nav button {
+    background: none;
+    border: none;
+    outline: none;
+    color: #bbb;
+    font-size: 15px;
+    text-transform: uppercase;
+    margin-right: 10px;
+    letter-spacing: 1px;
+    font-weight: bold;
+    cursor: pointer;
+}
+
+.filter-nav button.active {
+    color: #555;
+}
 </style>
