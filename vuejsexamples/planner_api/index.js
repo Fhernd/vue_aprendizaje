3bff4be941eb9df5f4c0c704273fe4b4b114ca7e
John Ortiz Ordoñez <johnortizo@outlook.com>
2023-09-29 16:49:06 -0500

    updated index

--- vuejsexamples/planner_api/index.js
+++ vuejsexamples/planner_api/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+
 app.use(express.json());
+
+app.use(cors());
 
 // Proyectos en Memoria
 let projects = [
