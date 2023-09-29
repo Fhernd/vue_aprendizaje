a714f5ab0a57c8f8407961998661684cc7567d2e
John Ortiz Ordoñez <johnortizo@outlook.com>
2023-09-29 08:53:21 -0500

    Filtrado de proyectos por estado

--- vuejsexamples/planner/src/views/HomePage.vue
+++ vuejsexamples/planner/src/views/HomePage.vue
@@ -37,5 +37,25 @@ export default {
             .catch(err => console.log(err.message))
             ;
     },
+    methods: {
+        deleteMethod(id) {
+            this.projects = this.projects.filter(project => project.id !== id);
+        },
+        completeMethod(id) {
+            const c = this.projects.find(project => project.id === id);
+            c.complete = !c.complete;
+        }
+    },
+    computed: {
+        filteredProjects() {
+            if (this.current === 'completed') {
+                return this.projects.filter(project => project.complete)
+            } else if (this.current === 'ongoing') {
+                return this.projects.filter(project => !project.complete);
+            } else {
+                return this.projects;
+            }
+        }
+    }
 }
 </script>
