4753afde39b3e08c0051ffbd43de2a41096cd2d6
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2023-09-28 19:25:57 -0500

    Styles for card

--- vue3_course/app003_notes/src/App.vue
+++ vue3_course/app003_notes/src/App.vue
@@ -16,38 +16,51 @@
 </template>
 
 <style scoped>
-  main {
-    height: 100hv;
-    width: 100vw;
-  }
+main {
+  height: 100hv;
+  width: 100vw;
+}
 
-  .container {
-    max-width: 1000px;
-    padding: 10px;
-    margin: 0 auto;
-  }
+.container {
+  max-width: 1000px;
+  padding: 10px;
+  margin: 0 auto;
+}
 
-  header {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-  }
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
 
-  h1 {
-    font-weight: bold;
-    margin-bottom: 25px;
-    font-size: 75px;
-  }
+h1 {
+  font-weight: bold;
+  margin-bottom: 25px;
+  font-size: 75px;
+}
 
-  button {
-    border: none;
-    padding: 10px;
-    width: 50px;
-    height: 50px;
-    cursor: pointer;
-    background-color: rgb(21, 20, 20);
-    border-radius: 100%;
-    color: white;
-    font-size: 20px;
-  }
+button {
+  border: none;
+  padding: 10px;
+  width: 50px;
+  height: 50px;
+  cursor: pointer;
+  background-color: rgb(21, 20, 20);
+  border-radius: 100%;
+  color: white;
+  font-size: 20px;
+}
+
+.card {
+  width: 225px;
+  height: 225px;
+  background-color: rgb(237, 182, 44);
+  padding: 10px;
+  border-radius: 15px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  margin-right: 20px;
+  margin-bottom: 20px;
+}
 </style>
