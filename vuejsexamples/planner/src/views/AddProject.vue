c40cbd38d34c88c481b0cd524e29255a844d8853
John Ortiz Ordoñez <johnortizo@outlook.com>
2023-09-29 09:14:27 -0500

    Template for adding a new project

---
+++ vuejsexamples/planner/src/views/AddProject.vue
@@ -0,0 +1,15 @@
+<template>
+    <form @submi.prevent="projectSubmit">
+        <label>Title:</label>
+        <br>
+        <input type="text" v-model="title" required>
+        <br>
+        <label>Details:</label>
+        <br>
+        <textarea v-model="details" required></textarea>
+        <br>
+        <button>Add Project</button>
+    </form>
+</template>
+
+
