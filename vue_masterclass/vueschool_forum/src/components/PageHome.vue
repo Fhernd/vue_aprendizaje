63206170d43f153855ead449c3fe3aa99b4e1e9c
John Ortiz Ordoñez <johnortizo@outlook.com>
2023-09-30 14:26:13 -0500

    First code organization

--- vue_masterclass/vueschool_forum/src/components/PageHome.vue
+++ vue_masterclass/vueschool_forum/src/components/PageHome.vue
@@ -5,7 +5,8 @@
     <div v-for="thread in threads" :key="thread.id">
         <h2>{{ thread.title }}</h2>
         <div v-for="postId in thread.posts" :key="postId">
-            <p>{{ posts.find(p => p.id == postId).text }}</p>
+            <p>{{ userById(postById(postId).userId).name }}</p>
+            <p>{{ postById(postId).text }}</p>
         </div>
     </div>
 </template>
@@ -19,6 +20,14 @@ export default {
             threads: sourceData.threads,
             posts: sourceData.posts,
             users: sourceData.users
+        }
+    },
+    methods: {
+        postById (id) {
+            return this.posts.find(p => p.id == id)
+        },
+        userById (id) {
+            return this.users.find(u => u.id == id)
         }
     }
 }
