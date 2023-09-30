1b0858c66711dabccc9c533b7a69924b3c86be5e
John Ortiz Ordoñez <johnortizo@outlook.com>
2023-09-30 14:20:06 -0500

    Iterating threads

--- vue_masterclass/vueschool_forum/src/components/PageHome.vue
+++ vue_masterclass/vueschool_forum/src/components/PageHome.vue
@@ -1,12 +1,21 @@
 <template>
     <div>
-        
+        Hello from PageHome
+    </div>
+    <div v-for="thread in threads" :key="thread.id">
+        <h2>{{ thread.title }}</h2>
     </div>
 </template>
 
 <script>
+import sourceData from '@/data.json'
+
 export default {
-    
+    data () {
+        return {
+            threads: sourceData.threads
+        }
+    }
 }
 </script>
 
