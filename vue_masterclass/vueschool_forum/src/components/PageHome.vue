ec1885f5cbb5ef07b4da6186bc4c7a1ffb144070
John Ortiz Ordoñez <johnortizo@outlook.com>
2023-09-30 15:11:26 -0500

    Better post presentation

--- vue_masterclass/vueschool_forum/src/components/PageHome.vue
+++ vue_masterclass/vueschool_forum/src/components/PageHome.vue
@@ -1,12 +1,29 @@
 <template>
-    <div>
-        Hello from PageHome
-    </div>
-    <div v-for="thread in threads" :key="thread.id">
-        <h2>{{ thread.title }}</h2>
-        <div v-for="postId in thread.posts" :key="postId">
-            <p>{{ userById(postById(postId).userId).name }}</p>
-            <p>{{ postById(postId).text }}</p>
+    <div v-for="thread in threads" :key="thread.id" class="col-large push-top">
+        <h1>{{ thread.title }}</h1>
+
+        <div class="post" v-for="postId in thread.posts" :key="postId">
+            <div class="user-info">
+                <a href="#" class="user-name">{{ userById(postById(postId).userId).name }}</a>
+
+                <a href="#">
+                    <img :src="userById(postById(postId).userId).avatar" alt="Avatar" class="avatar-large">
+                </a>
+
+                <p class="desktop-only text-small">100 posts</p>
+            </div>
+
+            <div class="post-content">
+                <div>
+                    <p>
+                        {{ postById(postId).text }}
+                    </p>
+                </div>
+            </div>
+
+            <div class="post-date text-faded">
+                <p>Posted on {{ postById(postId).publishedAt }}</p>
+            </div>
         </div>
     </div>
 </template>
@@ -15,7 +32,7 @@
 import sourceData from '@/data.json'
 
 export default {
-    data () {
+    data() {
         return {
             threads: sourceData.threads,
             posts: sourceData.posts,
@@ -23,16 +40,14 @@ export default {
         }
     },
     methods: {
-        postById (id) {
+        postById(id) {
             return this.posts.find(p => p.id == id)
         },
-        userById (id) {
+        userById(id) {
             return this.users.find(u => u.id == id)
         }
     }
 }
 </script>
 
-<style scoped>
-    
-</style>
+<style scoped></style>
