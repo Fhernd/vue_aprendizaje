773618b1ba603fb9a823a623613fe92c1456fea4
John Ortiz Ordoñez <johnortizo@outlook.com>
2023-10-01 09:50:17 -0500

    Using routing for components

--- vue_masterclass/vueschool_forum/src/main.js
+++ vue_masterclass/vueschool_forum/src/main.js
@@ -1,5 +1,19 @@
 import { createApp } from 'vue'
+
+import * as VueRouter from 'vue-router'
+
 import App from './App.vue'
+import PageHome from './components/PageHome.vue'
+
+const routes = [
+    { path: '/', component: PageHome }
+]
+
+const router = VueRouter.createRouter({
+    history: VueRouter.createWebHistory(),
+    routes
+});
 
 const forumApp = createApp(App);
+forumApp.use(router);
 forumApp.mount('#app');
