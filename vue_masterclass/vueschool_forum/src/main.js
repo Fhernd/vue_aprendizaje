33bb9439fa47555d981f63cf45393fde38b76483
John Ortiz Ordoñez <johnortizo@outlook.com>
2023-10-01 12:16:30 -0500

    New path on router for showing a specific thread

--- vue_masterclass/vueschool_forum/src/main.js
+++ vue_masterclass/vueschool_forum/src/main.js
@@ -4,13 +4,19 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 import App from './App.vue'
 import PageHome from './components/PageHome.vue'
+import PageThreadShow from './components/PageThreadShow.vue'
 
 const routes = [
-    { 
-        path: '/', 
-        name: 'home',
+    {
+        path: '/',
+        name: 'Home',
         component: PageHome
-     }
+    },
+    {
+        path: '/thread',
+        name: 'ThreadShow',
+        component: PageThreadShow
+    }
 ]
 
 const router = createRouter({
