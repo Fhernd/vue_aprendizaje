1153c4db5299d543d53b19d66001380a5e55e28b
John Ortiz Ordoñez <johnortizo@outlook.com>
2023-10-02 11:10:25 -0500

    Lista de threads con ThreadList

--- vue_masterclass/vueschool_forum/src/components/PageHome.vue
+++ vue_masterclass/vueschool_forum/src/components/PageHome.vue
@@ -1,37 +1,17 @@
 <template>
-    <div v-for="thread in threads" :key="thread.id" class="col-large push-top">
-        <h1>{{ thread.title }}</h1>
+    <h1>Welcome to the Forum</h1>
 
-        <div class="post" v-for="postId in thread.posts" :key="postId">
-            <div class="user-info">
-                <a href="#" class="user-name">{{ userById(postById(postId).userId).name }}</a>
-
-                <a href="#">
-                    <img :src="userById(postById(postId).userId).avatar" alt="Avatar" class="avatar-large">
-                </a>
-
-                <p class="desktop-only text-small">100 posts</p>
-            </div>
-
-            <div class="post-content">
-                <div>
-                    <p>
-                        {{ postById(postId).text }}
-                    </p>
-                </div>
-            </div>
-
-            <div class="post-date text-faded">
-                <p>Posted on {{ postById(postId).publishedAt }}</p>
-            </div>
-        </div>
-    </div>
+    <ThreadList />
 </template>
 
 <script>
+import ThreadList from '@/components/ThreadList.vue'
 import sourceData from '@/data.json'
 
 export default {
+    components: {
+        ThreadList
+    },
     data() {
         return {
             threads: sourceData.threads,
