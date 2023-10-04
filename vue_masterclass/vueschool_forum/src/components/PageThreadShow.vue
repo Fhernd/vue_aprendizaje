b06b3821830c417c7ed360b53f2c65f15bf4fe38
John Ortiz Ordoñez <johnortizo@outlook.com>
2023-10-04 11:22:44 -0500

    Thread no encontrado

--- vue_masterclass/vueschool_forum/src/components/PageThreadShow.vue
+++ vue_masterclass/vueschool_forum/src/components/PageThreadShow.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="col-large push-top">
+    <div v-if="thread" class="col-large push-top">
         <h1>{{ thread.title }}</h1>
 
         <div class="post" v-for="postId in thread.posts" :key="postId">
@@ -25,6 +25,10 @@
                 <p>Posted on {{ postById(postId).publishedAt }}</p>
             </div>
         </div>
+    </div>
+    <div v-else class="col-full text-center">
+        <h1>Not Found</h1>
+        <router-link :to="{name: 'Home'}">Thread not found</router-link>
     </div>
 </template>
 
