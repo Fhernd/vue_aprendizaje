76e4ee1c9d28853bdf4c4d53c690d7b773444ac5
John Ortiz Ordoñez <johnortizo@outlook.com>
2023-10-04 15:19:20 -0500

    Creating the PostList component

--- vue_masterclass/vueschool_forum/src/components/PostList.vue
+++ vue_masterclass/vueschool_forum/src/components/PostList.vue
@@ -1,12 +1,49 @@
-:<template>
-    <div>
-        
+<template>
+    <div class="post" v-for="post in posts" :key="post.id">
+        <div class="user-info">
+            <a href="#" class="user-name">{{ userById(post.userId).name }}</a>
+
+            <a href="#">
+                <img :src="userById(post.userId).userId).avatar" alt="Avatar" class="avatar-large">
+            </a>
+
+            <p class="desktop-only text-small">100 posts</p>
+        </div>
+
+        <div class="post-content">
+            <div>
+                <p>
+                    {{ postById(postId).text }}
+                </p>
+            </div>
+        </div>
+
+        <div class="post-date text-faded">
+            <p>Posted on {{ postById(postId).publishedAt }}</p>
+        </div>
     </div>
 </template>
 
 <script>
+import sourceData from '@/data.json'
+
 export default {
-    
+    props: {
+        thread: {
+            type: Array,
+            required: true
+        }
+    },
+    data() {
+        return {
+            users: sourceData.users
+        }
+    },
+    methods: {
+        userById(id) {
+            return this.users.find(u => u.id == id)
+        }
+    }
 }
 </script>
 
--- vue_masterclass/vueschool_forum/src/pages/ThreadShow.vue
+++ vue_masterclass/vueschool_forum/src/pages/ThreadShow.vue
@@ -3,30 +3,6 @@
         <h1>{{ thread.title }}</h1>
 
         <post-list @posts="threadPosts" />
-
-        <div class="post" v-for="postId in thread.posts" :key="postId">
-            <div class="user-info">
-                <a href="#" class="user-name">{{ userById(postById(postId).userId).name }}</a>
-
-                <a href="#">
-                    <img :src="userById(postById(postId).userId).avatar" alt="Avatar" class="avatar-large">
-                </a>
-
-                <p class="desktop-only text-small">100 posts</p>
-            </div>
-
-            <div class="post-content">
-                <div>
-                    <p>
-                        {{ postById(postId).text }}
-                    </p>
-                </div>
-            </div>
-
-            <div class="post-date text-faded">
-                <p>Posted on {{ postById(postId).publishedAt }}</p>
-            </div>
-        </div>
     </div>
 </template>
 
