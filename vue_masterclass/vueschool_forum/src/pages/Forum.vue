965a0a2e271786c6807a185229680622f5c9f3ff
John Ortiz Ordoñez <johnortizo@outlook.com>
2023-10-05 15:50:06 -0500

    Plantilla HTML inicial para la vista de foro

--- vue_masterclass/vueschool_forum/src/pages/Forum.vue
+++ vue_masterclass/vueschool_forum/src/pages/Forum.vue
@@ -1,12 +1,26 @@
 <template>
-    <div>
-        
+    <div class="col-full push-top">
+        <div class="forum-header">
+            <div class="forum-details">
+                <h1>Cooking</h1>
+                <p class="text-lead">Discuss your passion for food and cooking</p>
+            </div>
+            <a href="#" class="btn-green btn-small">Start a thread</a>
+        </div>
+    </div>
+    
+    <div class="col-full">
+        <ThreadList />
     </div>
 </template>
 
 <script>
+import ThreadList from '@/components/ThreadList.vue'
+
 export default {
-    
+    components: {
+        ThreadList
+    }
 }
 </script>
 
