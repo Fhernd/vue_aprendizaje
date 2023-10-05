8f65c16a648865af152a8bd82cb203570bf7b56a
John Ortiz Ordoñez <johnortizo@outlook.com>
2023-10-05 15:17:58 -0500

    parametrizacion componente para fechas

--- vue_masterclass/vueschool_forum/src/components/AppDate.vue
+++ vue_masterclass/vueschool_forum/src/components/AppDate.vue
@@ -1,12 +1,31 @@
 <template>
-    <div>
-        
-    </div>
+    <span :title="humanFriendlyDate()">
+        {{ diffForHumans() }}
+    </span>
 </template>
 
 <script>
+import dayjs from 'dayjs'
+import relativeTime from 'dayjs/plugin/relativeTime'
+import localizedFormat from 'dayjs/plugin/localizedFormat'
+dayjs.extend(relativeTime)
+dayjs.extend(localizedFormat)
+
 export default {
-    
+    props: {
+        timestamp: {
+            type: Number,
+            required: true
+        }
+    },
+    methods: {
+        diffForHumans() {
+            return dayjs(this.timestamp * 1000).fromNow()
+        },
+        humanFriendlyDate() {
+            return dayjs(this.timestamp * 1000).format('MMMM D, YYYY h:mm A')
+        }
+    }
 }
 </script>
 
--- vue_masterclass/vueschool_forum/src/components/PostList.vue
+++ vue_masterclass/vueschool_forum/src/components/PostList.vue
@@ -25,12 +25,6 @@
 </template>
 
 <script>
-import dayjs from 'dayjs'
-import relativeTime from 'dayjs/plugin/relativeTime'
-import localizedFormat from 'dayjs/plugin/localizedFormat'
-dayjs.extend(relativeTime)
-dayjs.extend(localizedFormat)
-
 import sourceData from '@/data.json'
 
 export default {
@@ -48,12 +42,6 @@ export default {
     methods: {
         userById(id) {
             return this.users.find(u => u.id == id)
-        },
-        diffForHumans(timestamp) {
-            return dayjs.unix(timestamp).fromNow()
-        },
-        humanFriendlyDate(timestamp) {
-            return dayjs.unix(timestamp).format('llll')
         }
     }
 }
