5c6cb6cb19559c11f94999cf3f49ac38e7c0f63c
John Ortiz Ordoñez <johnortizo@outlook.com>
2023-10-06 10:22:54 -0500

    Corrección de nombre de variable de iteración

--- vue_masterclass/vueschool_forum/src/components/ForumList.vue
+++ vue_masterclass/vueschool_forum/src/components/ForumList.vue
@@ -1,26 +1,25 @@
 <template>
-    <div class="forum-list">
-        <h2 class="list-title">
-            <a href="#">Forums</a>
-        </h2>
+    <div class="col-full">
+        <div class="forum-list">
+            <h2 class="list-title">
+                <a href="#">Forums</a>
+            </h2>
 
-        <div class="forum-listing" v-for="forum in forums" :key="forum.id">
-            <div class="forum-details">
-                <router-link :to="{ name: 'Forum', params: {id: forum.id}}" class="text-large">
-                    {{ form.name }}
-                </router-link>
-                <p>{{ forum.description }}</p>
-            </div>
-            <div class="threads-count">
-                <p><span class="count">{{ forum.threads.length }}</span> {{ forum.threads.length > 1 ? 'threads' : 'thread'
+            <div class="forum-listing" v-for="forum in forums" :key="forum.id">
+                <div class="forum-details">
+                    <router-link :to="{ name: 'Forum', params: { id: forum.id } }" class="text-large">
+                        {{ forum.name }}
+                    </router-link>
+                    <p>{{ forum.description }}</p>
+                </div>
+                <div class="threads-count">
+                    <p><span class="count">{{ forum.threads?.length }}</span> {{ forum.threads?.length > 1 ? 'threads' :
+                        'thread'
                     }}</p>
-            </div>
+                </div>
 
-            <div class="last-thread">
-                <img src="#" alt="" class="avatar">
-                <div class="thread-details">
-                    <a href="#">A##</a>
-                    <p class="text-xsmall">By <a href="#">A###</a>, a month ago</p>
+                <div class="last-thread">
+                    
                 </div>
             </div>
         </div>
