25c7b6aab0c93c58898d28a320c81c455e9d03e1
John Ortiz Ordoñez <johnortizo@outlook.com>
2023-10-07 14:35:50 -0500

    Resuelto el problema de carga de un nuevo post para un thread

--- vue_masterclass/vueschool_forum/src/store/index.js
+++ vue_masterclass/vueschool_forum/src/store/index.js
@@ -13,12 +13,12 @@ export default createStore({
         }
     },
     mutations: {
-        setPost(state, post) {
+        setPost(state, { post }) {
             state.posts.push(post);
         },
         appendPostToThread(state, { postId, threadId}) {
             const thread = state.threads.find(thread => thread.id === threadId)
-            thread.posts.push(postId)
+            thread.posts.push(postId);
         }
     }
 });
