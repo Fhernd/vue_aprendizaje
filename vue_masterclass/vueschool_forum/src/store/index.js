0ebcf9a300c3ea2975a8a9c0e594662a563f6f33
John Ortiz Ordoñez <johnortizo@outlook.com>
2023-10-07 09:27:58 -0500

    Actions y Mutations para el store

--- vue_masterclass/vueschool_forum/src/store/index.js
+++ vue_masterclass/vueschool_forum/src/store/index.js
@@ -3,5 +3,20 @@ import { createStore } from 'vuex'
 import sourceData from '@/data.json'
 
 export default createStore({
-    state: sourceData
+    state: sourceData,
+    actions: {
+        createPost(context, post) {
+            this.posts.push(post);
+            this.thread.posts.push(post);
+        }
+    },
+    mutations: {
+        setPosts(state, post) {
+            state.posts.push(post);
+        },
+        appendPostToThread(state, { postId, threadId}) {
+            const thread = state.threads.find(thread => thread.id === threadId)
+            thread.posts.push(postId)
+        }
+    }
 });
