79c74ad5b9ae6e09ed384434ad527627642d1f23
John Ortiz Ordoñez <johnortizo@outlook.com>
2023-10-10 15:52:04 -0500

    Barra de navegación primaria

--- vue_masterclass/vueschool_forum/src/components/TheNavbar.vue
+++ vue_masterclass/vueschool_forum/src/components/TheNavbar.vue
@@ -1,8 +1,36 @@
 <template>
      <header class="header" id="header">
-        <a href="index.html" class="logo">
-            <img src="" alt="Logo">
-        </a>
+        <router-link :to="{name: 'Home'}" class="logo">
+            <img src="../assets/svg/vueschool-logo.svg" alt="Logo">
+        </router-link>
+
+        <div class="btn-hamburger">
+            <div class="top bar"></div>
+            <div class="middle bar"></div>
+            <div class="bottom bar"></div>
+        </div>
+
+        <nav class="navbar">
+            <ul>
+                <li class="navbar-user">
+                    <a href="#">
+                        <img src="../assets/svg/arrow-profile.svg" alt="User">
+                        <span>
+                            John Ortiz Ordoñez
+                            <img src="../assets/svg/arrow-profile.svg" alt="User    " class="icon-profile">
+                        </span>
+                    </a>
+
+                    <div id="user-dropdown">
+                        <div class="triangle-menu"></div>
+                        <ul class="dropdown-menu">
+                            <li class="dropdown-menu-item"><a href="#">View profile</a></li>
+                            <li class="dropdown-menu-item"><a href="#">Log out</a></li>
+                        </ul>
+                    </div>
+                </li>
+            </ul>
+        </nav>
      </header>
 </template>
 
