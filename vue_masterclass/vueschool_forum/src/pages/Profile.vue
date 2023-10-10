fe77b3029f74cc32e2870037d760237b88ce92c8
John Ortiz Ordoñez <johnortizo@outlook.com>
2023-10-10 16:24:48 -0500

    Vista para el perfil del usuario

--- vue_masterclass/vueschool_forum/src/pages/Profile.vue
+++ vue_masterclass/vueschool_forum/src/pages/Profile.vue
@@ -6,8 +6,47 @@
                     <p class="text-center">
                         <img :src="user.avatar" :alt="`{user.name} profile picture`" class="avatar-xlarge">
                     </p>
+
+                    <h1 class="title">{{ user.username }}</h1>
+
+                    <p class="text-lead">{{ user.name }}</p>
+
+                    <p class="text-justify">
+                        {{ user.bio || 'No bio specified' }}
+                    </p>
+
+                    <span class="online">{{ usern.username }} is online</span>
+
+                    <div class="stats">
+                        <span>{{ userPostsCount }} posts</span>
+                        <span>{{ userThreadsCount }} threads</span>
+                    </div>
+
+                    <p v-if="user.website" class="text-large text-center">
+                        <i class="fa fa-globe"><a :href="user.webiste"> {{ user.website }}</a></i>
+                    </p>
+                </div>
+
+                <p class="text-xsmall text-faced text-center">
+                    Member since june 2003, last visited 4 hours ago
+                </p>
+
+                <div class="text-center">
+                    <hr>
+                    <a href="#" class="btn-green btn-small">Edit Profile</a>
                 </div>
             </div>
+
+            <div class="col-7 push-top">
+                <span class="text-lead">
+                    Joker's recent activity
+                </span>
+                <a href="#">See only started threads?</a>
+            </div>
+
+            <hr>
+
+            <PostLists :posts="userPosts" />
         </div>
     </div>
 </template>
