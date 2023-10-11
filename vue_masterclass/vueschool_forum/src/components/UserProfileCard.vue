6e5f57ac70c5599f31488a4bba4c807b8b610cf4
John Ortiz Ordoñez <johnortizo@outlook.com>
2023-10-11 15:35:26 -0500

    Reorganización de componentes

--- vue_masterclass/vueschool_forum/src/components/UserProfileCard.vue
+++ vue_masterclass/vueschool_forum/src/components/UserProfileCard.vue
@@ -1,12 +1,52 @@
 <template>
-    <div>
-        
+    <div class="profile-card">
+        <p class="text-center">
+            <img :src="user.avatar" :alt="`{user.name} profile picture`" class="avatar-xlarge">
+        </p>
+
+        <h1 class="title">{{ user.username }}</h1>
+
+        <p class="text-lead">{{ user.name }}</p>
+
+        <p class="text-justify">
+            {{ user.bio || 'No bio specified' }}
+        </p>
+
+        <span class="online">{{ user.username }} is online</span>
+
+        <div class="stats">
+            <span>{{ userPostsCount }} posts</span>
+            <span>{{ userThreadsCount }} threads</span>
+        </div>
+
+        <p v-if="user.website" class="text-large text-center">
+            <i class="fa fa-globe"><a :href="user.webiste"> {{ user.website }}</a></i>
+        </p>
     </div>
 </template>
 
 <script>
 export default {
-    
+    computed: {
+        userPosts() {
+            return this.$store.state.posts.filter(post => post.userId === this.user.id)
+        },
+        userPostsCount() {
+            return this.userPosts.length
+        },
+        userThreads() {
+            return this.$store.state.threads.filter(thread => thread.userId === this.user.id)
+        },
+        userThreadsCount() {
+            return this.userThreads.length
+        }
+    },
+    props: {
+        user: {
+            type: Object,
+            required: true
+        }
+    }
 }
 </script>
 
