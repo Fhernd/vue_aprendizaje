e85f727e6c23b29622d3ed4a8fde99a3b1a03ce5
John Ortiz Ordoñez <johnortizo@outlook.com>
2023-10-12 08:56:52 -0500

    Propiedad para el usuario activo para edición

--- vue_masterclass/vueschool_forum/src/components/UserProfileCardEditor.vue
+++ vue_masterclass/vueschool_forum/src/components/UserProfileCardEditor.vue
@@ -5,14 +5,14 @@
         </p>
 
         <div class="form-group">
-            <input type="text" class="form-input text-lead text-bold" placeholder="Username">
+            <input type="text" class="form-input text-lead text-bold" placeholder="Username" v-model="activeUser.username">
         </div>
         <div class="form-group">
-            <input type="text" class="form-input text-lead" placeholder="Full Name">
+            <input type="text" class="form-input text-lead" placeholder="Full Name" v-model="activeUser.name">
         </div>
         <div class="form-group">
             <label for="user_bio">Bio</label>
-            <textarea class="form-input" placeholder="Write a few words about yourself" id="user_bio"></textarea>
+            <textarea class="form-input" placeholder="Write a few words about yourself" id="user_bio" v-model="activeUser.bio"></textarea>
         </div>
         <div class="stats">
             <span>{{ user.postsCount }} posts</span>
@@ -23,12 +23,17 @@
 
         <div class="form-group">
             <label for="user_website" class="form-label">Website</label>
-            <input type="text" class="form-input" placeholder="https://example.com" id="user_website" autocomplete="off">
+            <input type="text" class="form-input" placeholder="https://example.com" id="user_website" autocomplete="off" v-model="activeUser.website">
+        </div>
+
+        <div class="form-group">
+            <label for="user_location" class="form-label">Location</label>
+            <input type="text" class="form-input" id="user_location" autocomplete="off" v-model="activeUser.location">
         </div>
 
         <div class="btn-group space-between">
             <button class="btn-ghost">Cancel</button>
-            <button type="submit" class="submit">Save</button>
+            <button type="submit" class="btn-blue">Save</button>
         </div>
     </div>
 </template>
@@ -39,6 +44,11 @@ export default {
         user: {
             type: Object,
             required: true
+        }
+    },
+    data() {
+        return {
+            activeUser: { ...this.user }
         }
     }
 }
