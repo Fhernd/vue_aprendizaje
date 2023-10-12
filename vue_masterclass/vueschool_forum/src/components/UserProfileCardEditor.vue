980786ac560c9edb2a56eec6c0d49a76c3a614cc
John Ortiz Ordoñez <johnortizo@outlook.com>
2023-10-12 09:01:12 -0500

    Formulario de edición de perfil

--- vue_masterclass/vueschool_forum/src/components/UserProfileCardEditor.vue
+++ vue_masterclass/vueschool_forum/src/components/UserProfileCardEditor.vue
@@ -1,40 +1,45 @@
 <template>
     <div class="profile-card">
-        <p class="text-center">
-            <img :src="user.avatar" :alt="`{user.name} profile picture`" class="avatar-xlarge img-update">
-        </p>
+        <form @submit.prevent="save">
+            <p class="text-center">
+                <img :src="user.avatar" :alt="`{user.name} profile picture`" class="avatar-xlarge img-update">
+            </p>
 
-        <div class="form-group">
-            <input type="text" class="form-input text-lead text-bold" placeholder="Username" v-model="activeUser.username">
-        </div>
-        <div class="form-group">
-            <input type="text" class="form-input text-lead" placeholder="Full Name" v-model="activeUser.name">
-        </div>
-        <div class="form-group">
-            <label for="user_bio">Bio</label>
-            <textarea class="form-input" placeholder="Write a few words about yourself" id="user_bio" v-model="activeUser.bio"></textarea>
-        </div>
-        <div class="stats">
-            <span>{{ user.postsCount }} posts</span>
-            <span>{{ user.threadsCount }} threads</span>
-        </div>
+            <div class="form-group">
+                <input type="text" class="form-input text-lead text-bold" placeholder="Username"
+                    v-model="activeUser.username">
+            </div>
+            <div class="form-group">
+                <input type="text" class="form-input text-lead" placeholder="Full Name" v-model="activeUser.name">
+            </div>
+            <div class="form-group">
+                <label for="user_bio">Bio</label>
+                <textarea class="form-input" placeholder="Write a few words about yourself" id="user_bio"
+                    v-model="activeUser.bio"></textarea>
+            </div>
+            <div class="stats">
+                <span>{{ user.postsCount }} posts</span>
+                <span>{{ user.threadsCount }} threads</span>
+            </div>
 
-        <hr />
+            <hr />
 
-        <div class="form-group">
-            <label for="user_website" class="form-label">Website</label>
-            <input type="text" class="form-input" placeholder="https://example.com" id="user_website" autocomplete="off" v-model="activeUser.website">
-        </div>
+            <div class="form-group">
+                <label for="user_website" class="form-label">Website</label>
+                <input type="text" class="form-input" placeholder="https://example.com" id="user_website" autocomplete="off"
+                    v-model="activeUser.website">
+            </div>
 
-        <div class="form-group">
-            <label for="user_location" class="form-label">Location</label>
-            <input type="text" class="form-input" id="user_location" autocomplete="off" v-model="activeUser.location">
-        </div>
+            <div class="form-group">
+                <label for="user_location" class="form-label">Location</label>
+                <input type="text" class="form-input" id="user_location" autocomplete="off" v-model="activeUser.location">
+            </div>
 
-        <div class="btn-group space-between">
-            <button class="btn-ghost">Cancel</button>
-            <button type="submit" class="btn-blue">Save</button>
-        </div>
+            <div class="btn-group space-between">
+                <button class="btn-ghost">Cancel</button>
+                <button type="submit" class="btn-blue">Save</button>
+            </div>
+        </form>
     </div>
 </template>
 
@@ -49,6 +54,11 @@ export default {
     data() {
         return {
             activeUser: { ...this.user }
+        }
+    },
+    methods: {
+        save() {
+            console.log('save', this.activeUser)
         }
     }
 }
