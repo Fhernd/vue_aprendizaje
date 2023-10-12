10ddd0fc0e0deddc4cfdb9f3845211922bc5e9b6
John Ortiz Ordoñez <johnortizo@outlook.com>
2023-10-12 16:32:58 -0500

    Realizando ajustes en el store

--- vue_masterclass/vueschool_forum/src/store/index.js
+++ vue_masterclass/vueschool_forum/src/store/index.js
@@ -43,6 +43,9 @@ export default createStore({
         setPost(state, { post }) {
             state.posts.push(post);
         },
+        setUser(state, { user, userId }) {
+            const userIndex = state.users.findIndex(user => user.id === userId);
+        },
         appendPostToThread(state, { postId, threadId }) {
             const thread = state.threads.find(thread => thread.id === threadId)
             thread.posts.push(postId);
