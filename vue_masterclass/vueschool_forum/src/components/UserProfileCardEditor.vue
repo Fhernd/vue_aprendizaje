5f2c6556f9361a87690eb528209947c0af2ac1a0
John Ortiz Ordoñez <johnortizo@outlook.com>
2023-10-12 08:47:44 -0500

    Primera versión de la plantilla de la tarjeta para la edición de un perfil

--- vue_masterclass/vueschool_forum/src/components/UserProfileCardEditor.vue
+++ vue_masterclass/vueschool_forum/src/components/UserProfileCardEditor.vue
@@ -1,12 +1,36 @@
 <template>
-    <div>
-        
+    <div class="profile-card">
+        <p class="text-center">
+            <img :src="user.avatar" :alt="`{user.name} profile picture`" class="avatar-xlarge img-update">
+        </p>
+
+        <div class="form-group">
+            <input type="text" class="form-input text-lead text-bold" placeholder="Username">
+        </div>
+        <div class="form-group">
+            <input type="text" class="form-input text-lead" placeholder="Full Name">
+        </div>
+        <div class="form-group">
+            <label for="user_bio">Bio</label>
+            <textarea class="form-input" placeholder="Write a few words about yourself" id="user_bio"></textarea>
+        </div>
+        <div class="stats">
+            <span>{{ user.postsCount }} posts</span>
+            <span>{{ user.threadsCount }} threads</span>
+        </div>
+
+        <hr />
+
+        <div class="form-group">
+            <label for="user_website" class="form-label">Website</label>
+            <input type="text" class="form-input" placeholder="https://example.com" id="user_website" autocomplete="off">
+        </div>
     </div>
 </template>
 
 <script>
 export default {
-    
+
 }
 </script>
 
