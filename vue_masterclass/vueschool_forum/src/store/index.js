ddd7e134006ca2a855b0a5557a381c7bd0ed218b
John Ortiz Ordoñez <johnortizo@outlook.com>
2023-10-13 11:36:33 -0500

    ¡Prettier es tremendo!

--- vue_masterclass/vueschool_forum/src/store/index.js
+++ vue_masterclass/vueschool_forum/src/store/index.js
@@ -1,84 +1,86 @@
-import { createStore } from 'vuex'
+import { createStore } from 'vuex';
 
-import sourceData from '@/data.json'
+import sourceData from '@/data.json';
 
 export default createStore({
-    state: {
-        ...sourceData,
-        authId: 'VXjpr2WHa8Ux4Bnggym8QFLdv5C3'
+  state: {
+    ...sourceData,
+    authId: 'VXjpr2WHa8Ux4Bnggym8QFLdv5C3',
+  },
+  getters: {
+    authUser: (state) => {
+      const user = state.users.find((user) => user.id === state.authId);
+
+      if (!user) return null;
+
+      return {
+        ...user,
+        get posts() {
+          return state.posts.filter((post) => post.userId === user.id);
+        },
+        get postsCount() {
+          return this.posts.length;
+        },
+        get threads() {
+          return state.threads.filter((thread) => thread.userId === user.id);
+        },
+        get threadsCount() {
+          return this.threads.length;
+        },
+      };
     },
-    getters: {
-        authUser: state => {
-            const user = state.users.find(user => user.id === state.authId)
-
-            if (!user) return null
-
-            return {
-                ...user,
-                get posts() {
-                    return state.posts.filter(post => post.userId === user.id)
-                },
-                get postsCount() {
-                    return this.posts.length
-                },
-                get threads() {
-                    return state.threads.filter(thread => thread.userId === user.id)
-                },
-                get threadsCount() {
-                    return this.threads.length
-                }
-            }
-        }
-    }
-    ,
-    actions: {
-        createPost({ commit, state }, post) {
-            post.id = 'greatPost' + Math.random();
-            post.publishedAt = Math.floor(Date.now() / 1000)
-            post.userId = state.authId;
-            commit('setPost', { post });
-            commit('appendPostToThread', { postId: post.id, threadId: post.threadId });
-        },
-        createThread({ commit, state}, {text, title}) {
-            const thread = {
-                "contributors": [
-                    "FsCDAk9w8NeXEceLV87arpsXjnQ2",
-                    "L664y3qZSubDbT1R6npC0EEybJ73",
-                    "VXjpr2WHa8Ux4Bnggym8QFLdv5C3"
-                ],
-                "firstPostId": "-KsjWehQ--apjDBwSBCZ",
-                "forumId": "-KsjPat5MWCx-dkjNVg8",
-                "lastPostAt": 1594040497,
-                "lastPostId": "-KsjnAAjzfpznUVh4DSa",
-                "posts": [
-                    "-KsjWehQ--apjDBwSBCZ",
-                    "-KsjefUiIuNkJieBc5Rq",
-                    "-KsjfvciJhJnebrL4Iw7",
-                    "-KsjnAAjzfpznUVh4DSa"
-                ],
-                "publishedAt": 1594035908,
-                "slug": "what-kind-of-pet-do-you-have",
-                "title": "What kind of pet do you have?",
-                "userId": "ALXhxjwgY9PinwNGHpfai6OWyDu2",
-                "id": "-KsjWehQ--apjDBwSBCY"
-            }
-        },
-        updateUser({ commit }, user) {
-            commit('setUser', { user, userId: user.id });
-        }
+  },
+  actions: {
+    createPost({ commit, state }, post) {
+      post.id = 'greatPost' + Math.random();
+      post.publishedAt = Math.floor(Date.now() / 1000);
+      post.userId = state.authId;
+      commit('setPost', { post });
+      commit('appendPostToThread', {
+        postId: post.id,
+        threadId: post.threadId,
+      });
     },
-    mutations: {
-        setPost(state, { post }) {
-            state.posts.push(post);
-        },
-        setUser(state, { user, userId }) {
-            const userIndex = state.users.findIndex(user => user.id === userId);
+    createThread({ commit, state }, { text, title }) {
+      const thread = {
+        contributors: [
+          'FsCDAk9w8NeXEceLV87arpsXjnQ2',
+          'L664y3qZSubDbT1R6npC0EEybJ73',
+          'VXjpr2WHa8Ux4Bnggym8QFLdv5C3',
+        ],
+        firstPostId: '-KsjWehQ--apjDBwSBCZ',
+        forumId: '-KsjPat5MWCx-dkjNVg8',
+        lastPostAt: 1594040497,
+        lastPostId: '-KsjnAAjzfpznUVh4DSa',
+        posts: [
+          '-KsjWehQ--apjDBwSBCZ',
+          '-KsjefUiIuNkJieBc5Rq',
+          '-KsjfvciJhJnebrL4Iw7',
+          '-KsjnAAjzfpznUVh4DSa',
+        ],
+        publishedAt: 1594035908,
+        slug: 'what-kind-of-pet-do-you-have',
+        title: 'What kind of pet do you have?',
+        userId: 'ALXhxjwgY9PinwNGHpfai6OWyDu2',
+        id: '-KsjWehQ--apjDBwSBCY',
+      };
+    },
+    updateUser({ commit }, user) {
+      commit('setUser', { user, userId: user.id });
+    },
+  },
+  mutations: {
+    setPost(state, { post }) {
+      state.posts.push(post);
+    },
+    setUser(state, { user, userId }) {
+      const userIndex = state.users.findIndex((user) => user.id === userId);
 
-            state.users[userIndex] = user;
-        },
-        appendPostToThread(state, { postId, threadId }) {
-            const thread = state.threads.find(thread => thread.id === threadId)
-            thread.posts.push(postId);
-        }
-    }
+      state.users[userIndex] = user;
+    },
+    appendPostToThread(state, { postId, threadId }) {
+      const thread = state.threads.find((thread) => thread.id === threadId);
+      thread.posts.push(postId);
+    },
+  },
 });
