0463e0c005bcd62561391f3326af10de4e77e3f8
John Ortiz Ordoñez <johnortizo@outlook.com>
2023-10-13 11:10:44 -0500

    Formulario para la creacion de un nuevo thread

--- vue_masterclass/vueschool_forum/src/pages/ThreadCreate.vue
+++ vue_masterclass/vueschool_forum/src/pages/ThreadCreate.vue
@@ -1,6 +1,23 @@
 <template>
-    <div>
-        
+    <div class="col-full push-top">
+        <h1>Create new thread in <i>{{ forum.name }}</i></h1>
+
+        <form @submit.prevent="save">
+            <div class="form-group">
+                <label for="thread_title">Title:</label>
+                <input type="text" class="form-input" id="thread_title" autocomplete="off" v-model="title">
+            </div>
+
+            <div class="form-group">
+                <label for="thread_content">Content:</label>
+                <textarea class="form-input" id="thread_content" rows="10" cols="140" v-model="content"></textarea>
+            </div>
+
+            <div class="btn-group">
+                <button class="btn btn-ghost">Cancel</button>
+                <button class="btn btn-blue" type="submit" name="Publish">Publish</button>
+            </div>
+        </form>
     </div>
 </template>
 
