5964f82a97b18c4749a1419a6a762d908be8d98b
John Ortiz Ordoñez <johnortizo@outlook.com>
2023-10-14 10:57:24 -0500

    Nueva ruta para navegación a la vista de creación de un thread

--- vue_masterclass/vueschool_forum/src/pages/Forum.vue
+++ vue_masterclass/vueschool_forum/src/pages/Forum.vue
@@ -1,43 +1,45 @@
 <template>
-    <div class="col-full push-top">
-        <div class="forum-header">
-            <div class="forum-details">
-                <h1>{{ forum.name }}</h1>
-                <p class="text-lead">{{ forum.description }}</p>
-            </div>
-            <a href="#" class="btn-green btn-small">Start a thread</a>
-        </div>
-    </div>
-    
-    <div class="col-full">
-        <ThreadList :threads="threads" />
-    </div>
+	<div class="col-full push-top">
+		<div class="forum-header">
+			<div class="forum-details">
+				<h1>{{ forum.name }}</h1>
+				<p class="text-lead">{{ forum.description }}</p>
+			</div>
+			<router-link
+				:to="{ name: 'ThreadCreate' }"
+				class="btn-green btn-small"
+				>Start a thread</router-link
+			>
+		</div>
+	</div>
+
+	<div class="col-full">
+		<ThreadList :threads="threads" />
+	</div>
 </template>
 
 <script>
 import ThreadList from '@/components/ThreadList.vue'
 
 export default {
-    components: {
-        ThreadList
-    },
-    props: {
-        id: {
-            type: String,
-            required: true
-        }
-    },
-    computed: {
-        forum() {
-            return this.$store.state.forums.find(f => f.id == this.id)
-        },
-        threads() {
-            return this.$store.state.threads.filter(t => t.forumId == this.id)
-        }
-    }
+	components: {
+		ThreadList,
+	},
+	props: {
+		id: {
+			type: String,
+			required: true,
+		},
+	},
+	computed: {
+		forum() {
+			return this.$store.state.forums.find((f) => f.id == this.id)
+		},
+		threads() {
+			return this.$store.state.threads.filter((t) => t.forumId == this.id)
+		},
+	},
 }
 </script>
 
-<style lang="">
-    
-</style>
+<style lang=""></style>
