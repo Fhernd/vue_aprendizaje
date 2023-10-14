2ee4bc4a0d989d603cce1b28a26295743b16e4c5
John Ortiz Ordoñez <johnortizo@outlook.com>
2023-10-14 10:52:42 -0500

    Persistencia desde la vista

--- vue_masterclass/vueschool_forum/src/pages/ThreadCreate.vue
+++ vue_masterclass/vueschool_forum/src/pages/ThreadCreate.vue
@@ -1,56 +1,73 @@
 <template>
-    <div class="col-full push-top">
-        <h1>Create new thread in <i>{{ forum.name }}</i></h1>
+	<div class="col-full push-top">
+		<h1>
+			Create new thread in <i>{{ forum.name }}</i>
+		</h1>
 
-        <form @submit.prevent="save">
-            <div class="form-group">
-                <label for="thread_title">Title:</label>
-                <input type="text" class="form-input" id="thread_title" autocomplete="off" v-model="title">
-            </div>
+		<form @submit.prevent="save">
+			<div class="form-group">
+				<label for="thread_title">Title:</label>
+				<input
+					type="text"
+					class="form-input"
+					id="thread_title"
+					autocomplete="off"
+					v-model="title"
+				/>
+			</div>
 
-            <div class="form-group">
-                <label for="thread_content">Content:</label>
-                <textarea class="form-input" id="thread_content" rows="10" cols="140" v-model="content"></textarea>
-            </div>
+			<div class="form-group">
+				<label for="thread_content">Content:</label>
+				<textarea
+					class="form-input"
+					id="thread_content"
+					rows="10"
+					cols="140"
+					v-model="content"
+				></textarea>
+			</div>
 
-            <div class="btn-group">
-                <button class="btn btn-ghost">Cancel</button>
-                <button class="btn btn-blue" type="submit" name="Publish">Publish</button>
-            </div>
-        </form>
-    </div>
+			<div class="btn-group">
+				<button class="btn btn-ghost">Cancel</button>
+				<button class="btn btn-blue" type="submit" name="Publish">
+					Publish
+				</button>
+			</div>
+		</form>
+	</div>
 </template>
 
 <script>
 export default {
-    props: {
-        forum: {
-            type: Object,
-            required: true
-        }
-    },
-    data() {
-        return {
-            title: '',
-            content: ''
-        }
-    },
-    methods: {
-        save() {
-            const thread = {
-                title: this.title,
-                content: this.content
-            }
-
-            this.$emit('save', { thread })
-
-            this.title = ''
-            this.content = ''
-        }
-    }
+	props: {
+		forum: {
+			type: Object,
+			required: true,
+		},
+	},
+	data() {
+		return {
+			title: '',
+			content: '',
+		}
+	},
+	methods: {
+		save() {
+			this.$store
+				.dispatch('createThread', {
+					forumId: this.forum.id,
+					title: this.title,
+					content: this.content,
+				})
+				.then(() => {
+					this.$router.push({
+						name: 'Forum',
+						params: { id: this.forum.id },
+					})
+				})
+		},
+	},
 }
 </script>
 
-<style lang="">
-    
-</style>
+<style lang=""></style>
