708f145322dd2792227e1d40ed75add4a7d0bce3
John Ortiz Ordoñez <johnortizo@outlook.com>
2023-10-14 11:36:06 -0500

    Uso componente para creación y edición de un thread

--- vue_masterclass/vueschool_forum/src/pages/ThreadCreate.vue
+++ vue_masterclass/vueschool_forum/src/pages/ThreadCreate.vue
@@ -4,41 +4,17 @@
 			Create new thread in <i>{{ forum.name }}</i>
 		</h1>
 
-		<form @submit.prevent="save">
-			<div class="form-group">
-				<label for="thread_title">Title:</label>
-				<input
-					type="text"
-					class="form-input"
-					id="thread_title"
-					autocomplete="off"
-					v-model="title"
-				/>
-			</div>
-
-			<div class="form-group">
-				<label for="thread_content">Content:</label>
-				<textarea
-					class="form-input"
-					id="thread_content"
-					rows="10"
-					cols="140"
-					v-model="content"
-				></textarea>
-			</div>
-
-			<div class="btn-group">
-				<button @click="cancel" class="btn btn-ghost">Cancel</button>
-				<button class="btn btn-blue" type="submit" name="Publish">
-					Publish
-				</button>
-			</div>
-		</form>
+		<ThreadEditor @save="save" @cancel="cancel" />
 	</div>
 </template>
 
 <script>
+import ThreadEditor from '@/components/ThreadEditor.vue'
+
 export default {
+	components: {
+		ThreadEditor,
+	},
 	computed: {
 		forum() {
 			return this.$store.state.forums.find((f) => f.id == this.forumId)
@@ -50,25 +26,19 @@ export default {
 			required: true,
 		},
 	},
-	data() {
-		return {
-			title: '',
-			content: '',
-		}
-	},
 	methods: {
-		async save() {
+		async save({ title, text }) {
 			const thread = await this.$store.dispatch('createThread', {
 				forumId: this.forum.id,
-				title: this.title,
-				content: this.content,
+				title,
+				text
 			})
 
 			this.$router.push({ name: 'ThreadShow', params: { id: thread.id } })
 		},
 		cancel() {
 			this.$router.push({ name: 'Forum', params: { id: this.forum.id } })
-		}
+		},
 	},
 }
 </script>
