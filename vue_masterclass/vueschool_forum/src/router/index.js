5e74752140b967e061f888894d17724095708723
John Ortiz Ordoñez <johnortizo@outlook.com>
2023-10-14 10:47:10 -0500

    Nueva configuración de formato de código

--- vue_masterclass/vueschool_forum/src/router/index.js
+++ vue_masterclass/vueschool_forum/src/router/index.js
@@ -10,75 +10,77 @@ import NotFound from '@/pages/NotFound.vue'
 import sourceData from '@/data.json'
 
 const routes = [
-    {
-        path: '/',
-        name: 'Home',
-        component: Home
-    },
-    {
-        path: '/me/edit',
-        name: 'ProfileEdit',
-        component: Profile,
-        props: { edit: true }
-    },
-    {
-        path: '/me',
-        name: 'Profile',
-        component: Profile,
-        meta: { toTop: true, smoothScroll: true }
-    },
-    {
-        path: '/category/:id',
-        name: 'Category',
-        component: Category,
-        props: true,
-    },
-    {
-        path: '/forum/:id',
-        name: 'Forum',
-        component: Forum,
-        props: true,
-    },
-    {
-        path: '/thread/show/:id',
-        name: 'ThreadShow',
-        component: ThreadShow,
-        props: true,
-        beforeEnter: (to, from, next) => {
-            const threadExist = sourceData.threads.find(thread => thread.id === to.params.id);
+	{
+		path: '/',
+		name: 'Home',
+		component: Home,
+	},
+	{
+		path: '/me/edit',
+		name: 'ProfileEdit',
+		component: Profile,
+		props: { edit: true },
+	},
+	{
+		path: '/me',
+		name: 'Profile',
+		component: Profile,
+		meta: { toTop: true, smoothScroll: true },
+	},
+	{
+		path: '/category/:id',
+		name: 'Category',
+		component: Category,
+		props: true,
+	},
+	{
+		path: '/forum/:id',
+		name: 'Forum',
+		component: Forum,
+		props: true,
+	},
+	{
+		path: '/thread/show/:id',
+		name: 'ThreadShow',
+		component: ThreadShow,
+		props: true,
+		beforeEnter: (to, from, next) => {
+			const threadExist = sourceData.threads.find(
+				(thread) => thread.id === to.params.id
+			)
 
-            if (threadExist) {
-                return next();
-            } else {
-                next({
-                    name: 'NotFound',
-                    params: { pathMatch: to.path.substring(1).split('/') },
-                    query: to.hash
-                });
-            }
-        }
-    },
-    {
-        path: '/:patchMatch(.*)*',
-        name: 'NotFound',
-        component: NotFound
-    }
+			if (threadExist) {
+				return next()
+			} else {
+				next({
+					name: 'NotFound',
+					params: { pathMatch: to.path.substring(1).split('/') },
+					query: to.hash,
+				})
+			}
+		},
+	},
+	{
+		path: '/:patchMatch(.*)*',
+		name: 'NotFound',
+		component: NotFound,
+	},
 ]
 
 export default createRouter({
-    history: createWebHistory(),
-    routes,
-    scrollBehavior(to, from, savedPosition) {
-        const scroll = {}
-        
-        if (to.meta.toTop) {
-            scroll.top = 0;
-        }
+	history: createWebHistory(),
+	routes,
+	scrollBehavior(to, from, savedPosition) {
+		const scroll = {}
 
-        if (to.meta.smoothScroll) {
-            scroll.behavior = 'smooth';
-        }
-        
-        return scroll;
-    }
-});
+		if (to.meta.toTop) {
+			scroll.top = 0
+		}
+
+		if (to.meta.smoothScroll) {
+			scroll.behavior = 'smooth'
+		}
+
+		return scroll
+	},
+})
