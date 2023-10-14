203de4783e4af1324eeeb002474526ff1544982b
John Ortiz Ordoñez <johnortizo@outlook.com>
2023-10-14 11:33:13 -0500

    Estructura general para guardar un thread

--- vue_masterclass/vueschool_forum/src/components/ThreadEditor.vue
+++ vue_masterclass/vueschool_forum/src/components/ThreadEditor.vue
@@ -1,15 +1,56 @@
 <template>
-    <div>
-        
-    </div>
+	<form @submit.prevent="save">
+		<div class="form-group">
+			<label for="thread_title">Title:</label>
+			<input
+				type="text"
+				class="form-input"
+				id="thread_title"
+				autocomplete="off"
+				v-model="title"
+			/>
+		</div>
+
+		<div class="form-group">
+			<label for="thread_content">Content:</label>
+			<textarea
+				class="form-input"
+				id="thread_content"
+				rows="10"
+				cols="140"
+				v-model="content"
+			></textarea>
+		</div>
+
+		<div class="btn-group">
+			<button @click.prevent="$email('cancel')" class="btn btn-ghost">Cancel</button>
+			<button class="btn btn-blue" type="submit" name="Publish">
+				Publish
+			</button>
+		</div>
+	</form>
 </template>
 
 <script>
 export default {
-    
+    data() {
+        return {
+            title: '',
+            content: '',
+        }
+    },
+    save() {
+        const thread = {
+            title: this.title,
+            content: this.content,
+        }
+
+        this.$emit('save', { thread })
+
+        this.title = ''
+        this.content = ''
+    },
 }
 </script>
 
-<style lang="">
-    
-</style>
+<style lang=""></style>
