d3416d70b5dca737b7a4a6e1d969e6a2c729ef65
John Ortiz Ordoñez <johnortizo@outlook.com>
2023-10-14 10:49:12 -0500

    Agregar un thread a un usuario

--- vue_masterclass/vueschool_forum/src/store/index.js
+++ vue_masterclass/vueschool_forum/src/store/index.js
@@ -1,81 +1,92 @@
-import { createStore } from 'vuex';
+import { createStore } from 'vuex'
 
-import sourceData from '@/data.json';
+import sourceData from '@/data.json'
 
 export default createStore({
-  state: {
-    ...sourceData,
-    authId: 'VXjpr2WHa8Ux4Bnggym8QFLdv5C3',
-  },
-  getters: {
-    authUser: (state) => {
-      const user = state.users.find((user) => user.id === state.authId);
+	state: {
+		...sourceData,
+		authId: 'VXjpr2WHa8Ux4Bnggym8QFLdv5C3',
+	},
+	getters: {
+		authUser: (state) => {
+			const user = state.users.find((user) => user.id === state.authId)
 
-      if (!user) return null;
+			if (!user) return null
 
-      return {
-        ...user,
-        get posts() {
-          return state.posts.filter((post) => post.userId === user.id);
-        },
-        get postsCount() {
-          return this.posts.length;
-        },
-        get threads() {
-          return state.threads.filter((thread) => thread.userId === user.id);
-        },
-        get threadsCount() {
-          return this.threads.length;
-        },
-      };
-    },
-  },
-  actions: {
-    createPost({ commit, state }, post) {
-      post.id = 'greatPost' + Math.random();
-      post.publishedAt = Math.floor(Date.now() / 1000);
-      post.userId = state.authId;
-      commit('setPost', { post });
-      commit('appendPostToThread', {
-        postId: post.id,
-        threadId: post.threadId,
-      });
-    },
-    createThread({ commit, state, dispatch }, { text, title, forumId }) {
-      const id = 'greatPost' + Math.random();
-      const userId = state.authId;
-      const publishedAt = Math.floor(Date.now() / 1000);
-      const thread = { forumId, title, publishedAt, userId, id };
+			return {
+				...user,
+				get posts() {
+					return state.posts.filter((post) => post.userId === user.id)
+				},
+				get postsCount() {
+					return this.posts.length
+				},
+				get threads() {
+					return state.threads.filter(
+						(thread) => thread.userId === user.id
+					)
+				},
+				get threadsCount() {
+					return this.threads.length
+				},
+			}
+		},
+	},
+	actions: {
+		createPost({ commit, state }, post) {
+			post.id = 'greatPost' + Math.random()
+			post.publishedAt = Math.floor(Date.now() / 1000)
+			post.userId = state.authId
+			commit('setPost', { post })
+			commit('appendPostToThread', {
+				postId: post.id,
+				threadId: post.threadId,
+			})
+		},
+		createThread({ commit, state, dispatch }, { text, title, forumId }) {
+			const id = 'greatPost' + Math.random()
+			const userId = state.authId
+			const publishedAt = Math.floor(Date.now() / 1000)
+			const thread = { forumId, title, publishedAt, userId, id }
 
-      commit('setThread', { thread });
+			commit('setThread', { thread })
 
-      dispatch('createPost', { text, threadId: thread.id });
-    },
-    updateUser({ commit }, user) {
-      commit('setUser', { user, userId: user.id });
-    },
-  },
-  mutations: {
-    setPost(state, { post }) {
-      state.posts.push(post);
-    },
-    setThread(state, { thread }) {
-      state.threads.push(thread);
-    },
-    setUser(state, { user, userId }) {
-      const userIndex = state.users.findIndex((user) => user.id === userId);
+			dispatch('createPost', { text, threadId: thread.id })
+		},
+		updateUser({ commit }, user) {
+			commit('setUser', { user, userId: user.id })
+		},
+	},
+	mutations: {
+		setPost(state, { post }) {
+			state.posts.push(post)
+		},
+		setThread(state, { thread }) {
+			state.threads.push(thread)
+		},
+		setUser(state, { user, userId }) {
+			const userIndex = state.users.findIndex(
+				(user) => user.id === userId
+			)
 
-      state.users[userIndex] = user;
-    },
-    appendPostToThread(state, { postId, threadId }) {
-      const thread = state.threads.find((thread) => thread.id === threadId);
-      thread.posts = thread.posts || [];
-      thread.posts.push(postId);
-    },
-    appendThreadToForum(state, { forumId, threadId }) {
-      const forum = state.forums.find((forum) => forum.id === forumId);
-      forum.threads = forum.threads || [];
-      forum.threads.push(threadId);
-    },
-  },
-});
+			state.users[userIndex] = user
+		},
+		appendPostToThread(state, { postId, threadId }) {
+			const thread = state.threads.find(
+				(thread) => thread.id === threadId
+			)
+			thread.posts = thread.posts || []
+			thread.posts.push(postId)
+		},
+		appendThreadToForum(state, { forumId, threadId }) {
+			const forum = state.forums.find((forum) => forum.id === forumId)
+			forum.threads = forum.threads || []
+			forum.threads.push(threadId)
+		},
+		appendThreadToUser(state, { userId, threadId }) {
+			const user = state.users.find((user) => user.id === userId)
+			user.threads = user.threads || []
+			user.threads.push(threadId)
+		}
+	},
+})
