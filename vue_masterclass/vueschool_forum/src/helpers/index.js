8e3f5a8d5ef3b981eaeb1e8dae510b357358d801
John Ortiz Ordoñez <johnortizo@outlook.com>
2023-10-21 07:32:17 -0500

    Función para insertar o actualizar un registro

--- vue_masterclass/vueschool_forum/src/helpers/index.js
+++ vue_masterclass/vueschool_forum/src/helpers/index.js
@@ -1,4 +1,11 @@
 export const findById = (resources, id) => resources.find((r) => r.id === id)
 
+export const upser = (resources, resource) => {
+    const index = resources.findIndex((r) => r.id === resource.id)
 
-
+    if (index !== -1) {
+        resources[index] = resource
+    } else {
+        resources.push(resource)
+    }
+}
